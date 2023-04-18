1958f850e030dd10ffb7fdbfcf611d454dd35b74
Daniela <daniela.espinosa.3103@gmail.com>
2023-04-18 08:01:53 -0500

    contectar bd

--- routes/user.py
+++ routes/user.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+from config.db import conn
+from models.user import users
 
 user = APIRouter()
 
 @user.get("/users")
-def helloworld():
-    return "hello world"
+def get_users():
+    return conn.execute(users.select()).fetch_all()
