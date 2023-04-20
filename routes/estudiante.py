45bd7010db065a0de08e49db1f70bef81ff3f102
Daniela Espinosa <daniela.espinosa.3103@gmail.com>
2023-04-19 19:52:22 -0600

    actualizar consulta?

--- routes/estudiante.py
+++ routes/estudiante.py
@@ -1,11 +1,11 @@
-#from distutils.log import error
-#from xmlrpc.client import SERVER_ERROR
-#from fastapi import APIRouter, Response, Header
-#from fastapi.responses import JSONResponse
-#from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
-#from werkzeug.security import generate_password_hash, check_password_hash
+# from distutils.log import error
+from xmlrpc.client import SERVER_ERROR
+# from fastapi import APIRouter, Response, Header
+# from fastapi.responses import JSONResponse
+# from starlette.status import HTTP_201_CREATED, HTTP_204_NO_CONTENT, HTTP_401_UNAUTHORIZED
+# from werkzeug.security import generate_password_hash, check_password_hash
 
-from config.db import conn#, engine
+from config.db import conn, engine
 from fastapi import APIRouter
 from models.estudiante import estudiantes
 from schemas.estudiante import Estudiante
@@ -16,12 +16,17 @@ estudianteRouter = APIRouter()
 
 @estudianteRouter.get("/estudiantes")
 def get_uestudiantes():
-    return conn.execute(estudiantes.select()).fetchall()
-
+    try:
+        with engine.connect() as conn:
+            result = conn.execute(estudiantes.select()).fetchall()
+    except Exception as exception_error:
+        return Response(status_code= SERVER_ERROR )
+   
 
 @estudianteRouter.post("/estudiantes")
 def create_estudiante(data_estudiante: Estudiante):
-    new_estudiante = {
+    with engine.connect() as conn:
+        new_estudiante = {
         "matricula": data_estudiante.matricula,
         "contraseña": data_estudiante.contraseña,
         "nombre": data_estudiante.nombre,
@@ -33,17 +38,29 @@ def create_estudiante(data_estudiante: Estudiante):
     }
 
     print(new_estudiante)
-#    return conn.execute(estudiantes.select().where(estudiantes.c.id == result.lastrowid)).first()
-    result_create = conn.execute(estudiantes.insert().values(
+#   return conn.execute(estudiantes.select().where(estudiantes.c.id == result.lastrowid)).first()
+    result_create=conn.execute(estudiantes.insert().values(
         matricula=data_estudiante.matricula,
         contraseña=data_estudiante.contraseña,
         nombre=data_estudiante.nombre,
         correo=data_estudiante.correo,
         semestre=int(data_estudiante.semestre),
         campus=data_estudiante.campus,
-        telefono=data_estudiante.telefono,
-        foto_perfil=data_estudiante.foto_perfil,
+        telefono=str(data_estudiante.telefono)
     ))
     print(result_create)
     print(result_create.lastrowid)
-    print(conn.execute(estudiantes.select().where(estudiantes.c.id == result_create.lastrowid)).first())
+    result = conn.execute(estudiantes.select().where(
+    estudiantes.c.id == result_create.lastrowid)).first()
+    print(result)
+
+    if result_create:
+        return {
+            "status": 200,
+            "message": "Access success",
+        }
+    else:
+        return {
+            "status": 404,
+            "message": "User not found"
+        }
